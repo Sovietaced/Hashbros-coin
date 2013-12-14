4aa2458d8ebb49ee926145179a33caf617864de9
Sovietaced <Sovietaced@gmail.com>
2013-12-14 05:29:52 -0500

    finally found a solution

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,9 @@
-set :host, ["litecoin.hashbros.co.in","galaxycoin.hashbros.co.in"]
-role :web, host                # Your HTTP server, Apache/etc
-role :app, host                # This may be the same as your `Web` server
-role :db, host, primary: true  # This is where Rails migrations will run
+servers = ["litecoin.hashbros.co.in","galaxycoin.hashbros.co.in"]
+servers.each do |server|
+	role :web, server                # Your HTTP server, Apache/etc
+	role :app, server                # This may be the same as your `Web` server
+	role :db, server, primary: true  # This is where Rails migrations will run
+end
 set :deploy_to, "/var/www/Hashbros-coin"
 set :rails_env, 'production'
 set :branch, 'master'
