f7705dee1db670639989a19a469d95afc190f822
Sovietaced <Sovietaced@gmail.com>
2013-12-14 05:23:13 -0500

    oops, need to make a list apparently

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :host, "litecoin.hashbros.co.in", "galaxycoin.hashbros.co.in"
+set :host, ["litecoin.hashbros.co.in","galaxycoin.hashbros.co.in"]
 role :web, host                # Your HTTP server, Apache/etc
 role :app, host                # This may be the same as your `Web` server
 role :db, host, primary: true  # This is where Rails migrations will run
