40f81557c241501057309a8b922a4550db7a767f
Sovietaced <Sovietaced@gmail.com>
2013-12-14 03:03:22 -0500

    more attempts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 require 'capistrano/ext/multistage'
+require "bundler/capistrano"
 
 set :application, "Hashbros-coin"
 set :stages, ["production"]
 set :default_stage, "production"
 
 set :scm, :git
-set :scm_user, "Sovietaced/Hashbros-coin"
 set :repository,  "git@github.com:Sovietaced/Hashbros-coin.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
@@ -15,6 +15,7 @@ set :use_sudo, false
 
 # So sudo password doesn't throw errors
 default_run_options[:pty] = true
+set :ssh_options, { :forward_agent => true }
 
 # After restart the web server
 after "deploy", "deploy:restart_daemons" 
