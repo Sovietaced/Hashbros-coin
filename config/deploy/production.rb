e024dc53027c418f89522806ac5540e3a9c6629a
Sovietaced <Sovietaced@gmail.com>
2013-12-14 03:19:37 -0500

    still no dice

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
-role :all, "litecoin.hashbros.co.in", :primary => true, :no_release => false
+set :host, "litecoin.hashbros.co.in"
+role :web, host                # Your HTTP server, Apache/etc
+role :app, host                # This may be the same as your `Web` server
+role :db, host, primary: true  # This is where Rails migrations will run
 set :deploy_to, "/var/www/Hashbros-coin"
 set :rails_env, 'production'
 set :branch, 'master'
