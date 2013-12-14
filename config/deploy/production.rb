ab44307aa5fdd288854ec46bd5edbdbfb6013451
Sovietaced <Sovietaced@gmail.com>
2013-12-14 03:18:26 -0500

    lets try this

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-role :all, "litecoin.hashbros.co.in", :primary => true
+role :all, "litecoin.hashbros.co.in", :primary => true, :no_release => false
 set :deploy_to, "/var/www/Hashbros-coin"
 set :rails_env, 'production'
 set :branch, 'master'
