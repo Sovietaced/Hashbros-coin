387d0306936a763f822bdd82cd767b85b4b87741
Sovietaced <Sovietaced@gmail.com>
2013-12-20 02:47:48 -0500

    added some basic views to spit out some test json

--- app/controllers/pool_worker_controller.rb
+++ app/controllers/pool_worker_controller.rb
@@ -1,6 +1,10 @@
 class PoolWorkerController < ApplicationController
 
+	def index
+		@worker = PoolWorker.first.to_json
+	end
+
 	def show
-		@worker = PoolWorker.find(params[:id])
+		@worker = PoolWorker.find(params[:id]).to_json
 	end
 end
--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,6 +1,10 @@
 class SharesController < ApplicationController
 
+	def index
+		@share = Share.first.to_json
+	end
+
 	def show
-		@share = Share.find(params[:id])
+		@share = Share.find(params[:id]).to_json
 	end
 end
