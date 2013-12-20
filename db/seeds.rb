5ff6976aee95dc880e9c10825568adbc73e54f37
Sovietaced <Sovietaced@gmail.com>
2013-12-20 02:35:20 -0500

    added db seed for dev mode

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Share.delete_all
+PoolWorker.delete_all
+
+(1..100).to_a.each do |i| 
+	Share.create!(:rem_host => '24.188.55.241', :username => 'travisby.1', :our_result => 'Y', :upstream_result => 'N', :solution => '000006536e7f2cdcd652629673bf930d23d8a7c53f638711fd36cb0713329b13', :time => '2013-12-15 02:42:33', :difficulty => 16)
+end
+
+PoolWorker.create!(:account_id => 0, :username => 'travisby.1', :password => 'nig', :hashrate => 540, :difficulty => 16)
