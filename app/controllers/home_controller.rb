82763976193167701e33fc77d51c3eda68108f01
Sovietaced <Sovietaced@gmail.com>
2013-12-23 03:54:45 -0500

    udated the seed file, finalized the summary api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,6 +23,28 @@ class HomeController < ApplicationController
   	# find all shares within the range <3 rails
   	round_shares = Share.where(:time => start..stop)
 
-  	render :json => round_shares
+  	num_round_shares_accepted = round_shares.where(:our_result => "Y").count
+  	num_round_shares_rejected = round_shares.where(:our_result => "N").count
+
+  	# determine the number of blocks found
+  	blocks = round_shares.where(:upstream_result => "Y").count
+
+  	# get a unique array of the workers involved
+  	workers = round_shares.pluck(:username).uniq
+
+  	work = {}
+
+  	workers.each do |worker|
+  		num_accepted = round_shares.where(:username => worker, :our_result => "Y").count
+  		num_rejected = round_shares.where(:username => worker, :our_result => "N").count
+  		# Calculate the percentage of work done, notice the float
+  		percentage_of_work = num_accepted.to_f / num_round_shares_accepted
+  		# Determine their reject rate for this round
+  		reject_rate = num_rejected.to_f / (num_accepted + num_rejected)
+
+  		work[worker] = {:accepted_shares => num_accepted, :rejected_shares => num_rejected, :work => percentage_of_work, :reject_rate => reject_rate}
+  	end
+
+  	render :json => {:total_shares => round_shares.count, :accepted_shares => num_round_shares_accepted, :rejected_shares => num_round_shares_rejected, :blocks => blocks, :work => work}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,15 @@
 Share.delete_all
 PoolWorker.delete_all
 
-(1..100).to_a.each do |i| 
+(1..1000).to_a.each do |i| 
 	Share.create!(:rem_host => '24.188.55.241', :username => 'travisby.1', :our_result => 'Y', :upstream_result => 'N', :solution => '000006536e7f2cdcd652629673bf930d23d8a7c53f638711fd36cb0713329b13', :time => '2013-12-15 02:42:33', :difficulty => 16)
 end
 
 PoolWorker.create!(:account_id => 0, :username => 'travisby.1', :password => 'nig', :hashrate => 540, :difficulty => 16)
+
+share = Share.last
+
+share.upstream_result = "Y"
+share.our_result = "N"
+
+share.save!
