3f734fc1965bea7f6cf797124c89824fcf496dc0
Sovietaced <Sovietaced@gmail.com>
2013-12-27 07:06:58 -0500

    add new serverS

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-servers = ["litecoin.hashbros.co.in","galaxycoin.hashbros.co.in", "dogecoin.hashbros.co.in"]
+servers = ["dogecoin.hashbros.co.in", "grandcoin.hashbros.co.in", "ps-franko.hashbros.co.in", "frank.hashbros.co.in", "ps-alphacoin.hashbros.co.in"]
 servers.each do |server|
 	role :web, server                # Your HTTP server, Apache/etc
 	role :app, server                # This may be the same as your `Web` server
