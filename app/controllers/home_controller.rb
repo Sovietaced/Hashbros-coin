f72f8659fd1d53c0adfb2516cb22cb683d70747b
Sovietaced <Sovietaced@gmail.com>
2014-01-04 07:05:27 -0800

    Fix reject rate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -79,7 +79,7 @@ class HomeController < ApplicationController
   	num_round_shares_accepted = round_shares.where(:our_result => "Y").count
   	num_round_shares_rejected = round_shares.where(:our_result => "N").count
 
-    reject_rate = (num_round_shares_rejected.to_f / round_shares.count) * 100
+    round_reject_rate = (num_round_shares_rejected.to_f / round_shares.count) * 100
 
 
     ''' Worker data '''
@@ -91,12 +91,13 @@ class HomeController < ApplicationController
   	workers.each do |worker|
   		num_accepted = round_shares.where(:username => worker, :our_result => "Y").count
   		num_rejected = round_shares.where(:username => worker, :our_result => "N").count
+      reject_rate = num_rejected.to_f / (num_accepted + num_rejected)
   		# Calculate the percentage of work done, notice the float
   		percentage_of_work = num_accepted.to_f / num_round_shares_accepted
 
   		work[worker] = {:accepted_shares => num_accepted, :rejected_shares => num_rejected, :percentage_of_work => percentage_of_work, :reject_rate => reject_rate}
   	end
 
-  	render :json => {:total_shares => round_shares.count, :accepted_shares => num_round_shares_accepted, :rejected_shares => num_round_shares_rejected, :work => work, :reject_rate => reject_rate}
+  	render :json => {:total_shares => round_shares.count, :accepted_shares => num_round_shares_accepted, :rejected_shares => num_round_shares_rejected, :work => work, :reject_rate => round_reject_rate}
   end
 end
