9e89adbc515df0a5a77bf99c83859933119c9615
Sovietaced <Sovietaced@gmail.com>
2014-01-11 14:25:27 -0800

    fuck anoncoin

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-servers = ["ps-anoncoin.hashbros.co.in", "ps-netcoin.hashbros.co.in", "ps-digitalcoin.hashbros.co.in", "ps-dogecoin.hashbros.co.in", "ps-fastcoin.hashbros.co.in", "ps-worldcoin.hashbros.co.in", "ps-spots.hashbros.co.in", "ps-lottocoin.hashbros.co.in", "ps-casinocoin.hashbros.co.in", "ps-grandcoin.hashbros.co.in", "ps-franko.hashbros.co.in"]
+servers = ["162.243.220.129", "ps-netcoin.hashbros.co.in", "ps-digitalcoin.hashbros.co.in", "ps-dogecoin.hashbros.co.in", "ps-fastcoin.hashbros.co.in", "ps-worldcoin.hashbros.co.in", "ps-spots.hashbros.co.in", "ps-lottocoin.hashbros.co.in", "ps-casinocoin.hashbros.co.in", "ps-grandcoin.hashbros.co.in", "ps-franko.hashbros.co.in"]
 servers.each do |server|
 	role :web, server                # Your HTTP server, Apache/etc
 	role :app, server                # This may be the same as your `Web` server
