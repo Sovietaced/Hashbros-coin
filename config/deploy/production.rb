5cbcd190f2e8cb7668522e68ddf5aa7a8aa791e8
Sovietaced <Sovietaced@gmail.com>
2014-01-11 03:09:47 -0800

    add anoncoin

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-servers = ["ps-netcoin.hashbros.co.in", "162.243.31.123", "ps-dogecoin.hashbros.co.in", "ps-fastcoin.hashbros.co.in", "ps-worldcoin.hashbros.co.in", "ps-spots.hashbros.co.in", "ps-lottocoin.hashbros.co.in", "ps-casinocoin.hashbros.co.in", "ps-grandcoin.hashbros.co.in", "ps-franko.hashbros.co.in"]
+servers = ["ps-anoncoin.hashbros.co.in", "ps-chinacoin.hashbros.co.in", "ps-netcoin.hashbros.co.in", "ps-digitalcoin.hashbros.co.in", "ps-dogecoin.hashbros.co.in", "ps-fastcoin.hashbros.co.in", "ps-worldcoin.hashbros.co.in", "ps-spots.hashbros.co.in", "ps-lottocoin.hashbros.co.in", "ps-casinocoin.hashbros.co.in", "ps-grandcoin.hashbros.co.in", "ps-franko.hashbros.co.in"]
 servers.each do |server|
 	role :web, server                # Your HTTP server, Apache/etc
 	role :app, server                # This may be the same as your `Web` server
