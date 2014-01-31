eee380216229a4219a54d71ea633aafaf7241b99
Sovietaced <Sovietaced@gmail.com>
2014-01-31 00:42:53 -0800

    OPTIMIZED CODE

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -93,42 +93,6 @@ class HomeController < ApplicationController
 
 
     ''' Worker data '''
-  	# get a unique array of the workers involved
-  	workers = round_shares.pluck(:username).uniq
-
-  	work = {}
-
-  	workers.each do |worker|
-  		num_accepted = round_shares.where(:username => worker, :our_result => "Y").count
-  		num_rejected = round_shares.where(:username => worker, :our_result => "N").count
-      reject_rate = num_rejected.to_f / (num_accepted + num_rejected)
-  		# Calculate the percentage of work done, notice the float
-  		percentage_of_work = num_accepted.to_f / num_round_shares_accepted
-
-  		work[worker] = {:accepted_shares => num_accepted, :rejected_shares => num_rejected, :percentage_of_work => percentage_of_work, :reject_rate => reject_rate}
-  	end
-
-  	render :json => {:total_shares => round_shares.count, :accepted_shares => num_round_shares_accepted, :rejected_shares => num_round_shares_rejected, :work => work, :reject_rate => round_reject_rate}
-  end
-
-  # Start and Stop times in integer (string) epoch time
-  def test
-    
-    # parse parameters to datetime objects
-    start = Time.at(params[:start].to_i)
-    stop = Time.at(params[:stop].to_i)
-    time = start..stop
-
-    # find all shares within the range <3 rails
-    round_shares = Share.where(:time => start..stop)
-
-    num_round_shares_accepted = round_shares.where(:our_result => "Y").count
-    num_round_shares_rejected = round_shares.where(:our_result => "N").count
-
-    round_reject_rate = (num_round_shares_rejected.to_f / round_shares.count) * 100
-
-
-    ''' Worker data '''
     work = {}
 
     round_shares.each do |share|
