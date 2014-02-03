add105085990ab196146dd650a260774f2e9aa87
Sovietaced <Sovietaced@gmail.com>
2014-02-02 20:52:04 -0800

    starcoin

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,8 @@ servers = ["ps-megacoin.hashbros.co.in",
 	"ps-grandcoin.hashbros.co.in", 
 	"ps-franko.hashbros.co.in",
 	"ps-luckycoin.hashbros.co.in",
-    "ps-alphacoin.hashbros.co.in"]
+    "ps-alphacoin.hashbros.co.in",
+    "ps-starcoin.hashbros.co.in"]
 servers.each do |server|
 	role :web, server                # Your HTTP server, Apache/etc
 	role :app, server                # This may be the same as your `Web` server
