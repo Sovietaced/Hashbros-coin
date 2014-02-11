ef2cf9853e778165062baf2b8149268cf330485c
Sovietaced <Sovietaced@gmail.com>
2014-02-11 02:24:32 -0800

    update server list

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,28 +1,14 @@
-servers = ["ps-megacoin.hashbros.co.in", 
+servers = [
 	"ps-goldcoin.hashbros.co.in", 
-	"ps-philosopherstone.hashbros.co.in", 
-	"ps-neocoin.hashbros.co.in", 
-	"ps-ronpaulcoin.hashbros.co.in", 
-	"ps-mooncoin.hashbros.co.in", 
 	"ps-globalcoin.hashbros.co.in", 
-	"ps-litecoin.hashbros.co.in", 
-	"162.243.231.168", 
-	"107.170.11.219", 
-	"ps-anoncoin.hashbros.co.in", 
-	"ps-netcoin.hashbros.co.in", 
-	"ps-digitalcoin.hashbros.co.in", 
-	"ps-dogecoin.hashbros.co.in", 
+	"ps-digitalcoin.hashbros.co.in",  
 	"ps-fastcoin.hashbros.co.in", 
-	"ps-worldcoin.hashbros.co.in", 
-	"ps-spots.hashbros.co.in", 
-	"ps-lottocoin.hashbros.co.in", 
 	"ps-casinocoin.hashbros.co.in", 
 	"ps-grandcoin.hashbros.co.in", 
 	"ps-franko.hashbros.co.in",
 	"ps-luckycoin.hashbros.co.in",
     "ps-alphacoin.hashbros.co.in",
-    "ps-starcoin.hashbros.co.in",
-	"ps-kittehcoin.hashbros.co.in"]
+	"ps-stablecoin.hashbros.co.in"]
 servers.each do |server|
 	role :web, server                # Your HTTP server, Apache/etc
 	role :app, server                # This may be the same as your `Web` server
